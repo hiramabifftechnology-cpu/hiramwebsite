aaccb536afa645b107eb87de4eb338c4c98dfcd3
hiramabifftechnology-cpu <hiramabiff.technology@gmail.com>
2025-08-14 17:46:01 +0200

    Update team.ts

--- src/data/team.ts
+++ src/data/team.ts
@@ -3,30 +3,30 @@ import { TeamMember } from '../types';
 export const team: TeamMember[] = [
   {
     id: 1,
-    name: "Jean-Marc Ndongo",
+    name: "Xhaka Memang Ismael",
     position: "Directeur Général",
     image: "https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=400",
-    bio: "Ingénieur en génie civil avec 15 ans d'expérience dans la construction au Cameroun."
+    bio: "Ingénieur en Mecatronique automobile avec 5 ans d'expérience dans la construction au Cameroun."
   },
   {
     id: 2,
-    name: "Marie Atangana",
-    position: "Architecte en Chef",
+    name: "Donovan Ovambe",
+    position: " Chef Finance et superviseur architectural",
     image: "https://images.pexels.com/photos/3756681/pexels-photo-3756681.jpeg?auto=compress&cs=tinysrgb&w=400",
-    bio: "Architecte diplômée, spécialisée dans l'architecture moderne et écologique."
+    bio: "diplômée en Finance, ses spécialisée dans l'architecture moderne et écologique."
   },
   {
     id: 3,
-    name: "Paul Mbarga",
-    position: "Chef de Chantier",
+    name: "Tamah Nganou Wilbrandt Ismael",
+    position: "Chef de chantier",
     image: "https://images.pexels.com/photos/2182975/pexels-photo-2182975.jpeg?auto=compress&cs=tinysrgb&w=400",
-    bio: "Expert en gestion de chantier avec une grande expérience dans les gros œuvres."
+    bio: "Expert en gestion de chantier et System Intelligent avec une grande expérience dans les gros œuvres."
   },
   {
     id: 4,
-    name: "Stephanie Fouda",
-    position: "Responsable Qualité",
+    name: "Cherif Soulemane Mfossa",
+    position: "Chef Département Informatique",
     image: "https://images.pexels.com/photos/3756679/pexels-photo-3756679.jpeg?auto=compress&cs=tinysrgb&w=400",
-    bio: "Ingénieure qualité, garante du respect des normes et des délais de livraison."
+    bio: "Ingénieure en Sécurité Reseau et qualité, garante du respect des normes et des délais de livraison."
   }
 ];
